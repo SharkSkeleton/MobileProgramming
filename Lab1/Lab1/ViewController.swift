f29e8b6ac77b8a5113f6dcb6d9df4305292f701f
SharkSkeleton <mishamyagkiy3@gmail.com>
2019-04-22 14:23:20 +0300

    My initial project setup

---
+++ Lab1/Lab1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab1
+//
+//  Created by Mishka on 4/22/19.
+//  Copyright © 2019 Mishka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
